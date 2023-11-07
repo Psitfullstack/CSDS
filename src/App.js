412de7676193636c477c99ce0856b84c46e51479
Amir Khan <amirkhan1985@gmail.com>
2023-11-07 12:39:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
-
-
+import Home from './components/Home'
 
 function App() {
   return (
     <>  
    
    <Nav/>
+   <Home/>
   
    </>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ function Nav(){
 
     return(
         <>
-         <div className="container">
+         <div className="container1">
           <nav>
             <ul>
                 <li><a href="">Home</a></li>
